427f5d96260e5663e196d59d38557e243bdc21c2
Zheng Zhou <bevischou@outlook.com>
2022-12-06 14:36:14 +0000

    Celery beat setup

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -132,3 +132,10 @@ CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_TASK_SERIALIZER = 'json'
+
+CELERY_BEAT_SCHEDULE = {
+    'demo-every-second': {
+       'task': 'puller.tasks.demo',
+       'schedule': 1,
+    },
+}
