2c153b4384885f593a05936c65c6272d1116d91f
Zheng Zhou <bevischou@outlook.com>
2022-12-10 03:41:23 +0000

    Setup models

--- mysite/puller/models.py
+++ mysite/puller/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+class Watched(models.Model):
+    repo_path = models.CharField('path')
+    updated_till = models.DateField('date')
+
+
+class Actor(models.Model):
+    id = models.CharField(primary_key=True)
+    company = models.CharField()
+    url = models.CharField()
+
+
+class Repo(models.Model):
+    id = models.CharField(primary_key=True)
+    full_name = models.CharField()
+    url = models.CharField()
+
+
+class Event(models.Model):
+    class EventType(models.TextChoices):
+        CommitCommentEvent = 'CC'
+        ForkEvent = 'F'
+        IssueCommentEvent = 'IC'
+        IssuesEvent = 'I'
+        MemberEvent = 'M'
+        PullRequestEvent = 'PR'
+        PullRequestReviewEvent = 'PRR'
+        PullRequestReviewCommentEvent = 'PRRC'
+        PullRequestReviewThreadEvent = 'PRRT'
+        PushEvent = 'P'
+        WatchEvent = 'W'
+
+    event_type = models.CharField(
+        max_length=4,
+        choices=EventType.choices,
+    )
+    actor = models.ForeignKey(
+        Actor,
+        on_delete=models.CASCADE,
+    )
+    repo = models.ForeignKey(
+        Repo,
+        on_delete=models.CASCADE,
+    )
+    payload = models.TextField()
