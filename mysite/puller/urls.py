d1434a78e135d0276842f5900b213676509d4e96
Zheng Zhou <bevischou@outlook.com>
2022-12-13 16:00:12 +0000

    Added API

--- mysite/puller/urls.py
+++ mysite/puller/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('<str:owner>/<str:repo>/pr_per_month/<int:year>/', views.pull_request_per_month),
     path('<str:owner>/<str:repo>/star_per_month/<int:year>/', views.star_per_month),
+    path('<str:owner>/<str:repo>/commit_per_month/<int:year>/', views.commit_per_month),
+    path('<str:owner>/<str:repo>/collaborator_per_month/<int:year>/', views.collaborator_per_month),
 ]
--- mysite/puller/views.py
+++ mysite/puller/views.py
@@ -1,8 +1,14 @@
 from django.http import JsonResponse
 
 from puller.models import Event, Repo
+from puller.tasks import Fetcher
 
-# Create your views here.
+# Snapshot data.
+def dummy(request, owner, repo):
+    return JsonResponse({})
+
+
+# Incremental data.
 def pull_request_per_month(request, owner, repo, year):
     full_name = f"{owner}/{repo}"
     repo = Repo.objects.get(full_name=full_name)
@@ -19,3 +25,27 @@ def star_per_month(request, owner, repo, year):
     events = Event.objects.filter(created_at__year=year).filter(repo=repo).filter(type=Event.EventType.Watch)
     starred = [events.filter(created_at__month=i+1).count() for i in range(0, 12)]
     return JsonResponse({'starred': starred})
+
+
+def commit_per_month(request, owner, repo, year):
+    full_name = f"{owner}/{repo}"
+    repo = Repo.objects.get(full_name=full_name)
+    all_events = Event.objects.filter(created_at__year=year).filter(repo=repo).filter(type=Event.EventType.Push)
+    committed = []
+    for i in range(0, 12):
+        cnt = 0
+        events = all_events.filter(created_at__month=i+1)
+        for event in events:
+            cnt += int(event.additional_info)
+        committed.append(cnt)
+    return JsonResponse({'pushed': committed})
+
+
+def collaborator_per_month(request, owner, repo, year):
+    full_name = f"{owner}/{repo}"
+    repo = Repo.objects.get(full_name=full_name)
+    events = Event.objects.filter(created_at__year=year).filter(repo=repo).filter(type=Event.EventType.Member)
+    events_per_months = [events.filter(created_at__month=i+1) for i in range(0, 12)]
+    added = [events_in_month.filter(action=Event.Action.Added).count() for events_in_month in events_per_months]
+    removed = [events_in_month.filter(action=Event.Action.Removed).count() for events_in_month in events_per_months]
+    return JsonResponse({'added': added, 'removed': removed})
